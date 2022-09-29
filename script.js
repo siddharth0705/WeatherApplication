a221dc30f2bca60e5c62b599132cc7851746b6df
Siddharth Singh <55996069+siddharth0705@users.noreply.github.com>
2022-09-29 22:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,16 +39,10 @@ let weather = {
     },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-    weather.search();
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
 });
-
-document
-    .querySelector(".search-bar")
-    .addEventListener("keyup", function (event) {
-        if (event.key == "Enter") {
-            weather.search();
-        }
-    });
 
 weather.fetchWeather("New Delhi");
